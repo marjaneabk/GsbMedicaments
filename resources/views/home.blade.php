fcc1edac8ea0e74c3070799221a55dd0c40dca69
marjaneAbk <marjane.aberkan@lmdsio.fr>
2024-03-15 10:46:57 +0100

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+
+@extends('layouts.master')
+@section('content')
+    <div>
+
+        <br>
+        <br>
+        <br>
+        <h1 class="bvn">Gestion de la formulation des médicaments</h1>
+    </div>
+@stop
+
+
---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GSB Frais</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/monStyle.css') }}">
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <link href="//fonts.googleapis.com/css?family=Roboto:400,300" rel="stylesheet" type="text/css">
+    <style>
+
+
+        .navbar {
+            margin-bottom: 0;
+            background-color: #f8f8f8;
+            border-color: #e7e7e7;
+        }
+
+        .navbar-fixed-top {
+            border: none;
+        }
+
+
+        .navbar-nav > li > a {
+            color: #333;
+        }
+
+        .navbar-nav > li > a:hover,
+        .navbar-nav > li > a:focus {
+            color: #555;
+            background-color: transparent;
+        }
+
+        .container {
+            margin-top: 20px;
+        }
+
+
+    </style>
+</head>
+<body class="body">
+<div class="container">
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse"
+                        data-target="#navbar-collapse-target">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="{{ url('/') }}">GSB Frais</a>
+            </div>
+            @if (Session::get('id') == 0)
+                <div class="collapse navbar-collapse navbar-right">
+                    <ul class="nav navbar-nav">
+                        <li><a href="{{ url('/formLogin') }}" data-toggle="collapse"
+                               data-target=".navbar-collapse.in">Se connecter</a></li>
+                    </ul>
+                </div>
+            @endif
+            @if (Session::get('id') > 0)
+                <div class="collapse navbar-collapse" id="navbar-collapse-target">
+                    <ul class="nav navbar-nav">
+                        <li><a href="{{ url('/') }}" data-toggle="collapse"
+                               data-target=".navbar-collapse.in">Lister</a></li>
+                        <li><a href="{{ url('/') }}" data-toggle="collapse"
+                               data-target=".navbar-collapse.in">Ajouter</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="{{ url('/') }}" data-toggle="collapse"
+                               data-target=".navbar-collapse.in">Se déconnecter</a></li>
+                    </ul>
+                </div>
+            @endif
+        </div>
+    </nav>
+</div>
+<div class="container">
+    @yield('content')
+</div>
+
+<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('assets/js/bootstrap.js') }}"></script>
+</body>
+</html>
